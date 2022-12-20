69c52d4a876fb2df5d0e640d12f648e3b8ca2ae3
Daniel <danielzheng2@gmail.com>
2022-12-19 19:01:16 -0500

    addition changes

--- src/component/Loading.js
+++ src/component/Loading.js
@@ -3,8 +3,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Loading = () =>{
     return (
-        <div className="vh-100 bg-danger">
-            <h1>Loading</h1><div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+        <div className="vh-100 bg-danger text-white">
+            <h1>Loading</h1>
+            <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
         </div>
     );
 }
